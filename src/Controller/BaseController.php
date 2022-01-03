2cd45a72cad8b640319d9572ec59f9e8582cee10
root <root@server.sebastian-christoph.de>
2022-01-03 21:37:01 +0100

    impressum

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -8,11 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
-    #[Route('/', name: 'base')]
+    /**
+     * @Route("/", name="base")
+     */
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
             'controller_name' => 'BaseController',
         ]);
     }
+
+    /**
+     * @Route("/impressum", name="impressum")
+     */
+    public function impressum(): Response
+    {
+        return $this->render('base/impressum.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+
 }
